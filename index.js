5e2235a138559e4e51165cf56bc48f6d9bc84cc9
Desmond Yeoh <desmondyeoh11@gmail.com>
2017-12-15 01:16:52 +0800

    Refactor code, modularizes (State, LocalStorage, Main, NodeView, helpers Fx), Add 'Clear' button

--- index.js
+++ index.js
@@ -1,16 +1,162 @@
-var N_KEY = 110;
-var STATES_DOMID = 'states';
-var CANVAS_DOMID = 'big-canvas';
-var STATES = {};
-
-window.onload = main;
+var DEBUG_MODE = true;
+var m = new Main()
+window.onload = function() {m.main()};
 
 
-function NodeView(nodeId, position) {
-	// DEFAULT VALUES
+
+/*************************************************************
+* MAIN MODULE
+*************************************************************/
+
+function Main() {
+	this.state = new State(cnst('domId/states'));
+	this.ls = new LocalStorage(this.state, cnst('domId/canvas'));
+}
+
+Main.prototype.main = function() {
+	// SET INITIAL STATE
+	this.state.set("nodes", {});
+	this.state.set("mouseDown/targetId", null);
+	this.state.set("mode", "idle");
+
+	// LOAD FROM LOCAL STORAGE
+	this.ls.load();
+
+	this.handleKeyPress = this.handleKeyPress.bind(this);
+	this.handleClick = this.handleClick.bind(this);
+	this.handleDblClick = this.handleDblClick.bind(this);
+	this.handleMouseDown = this.handleMouseDown.bind(this);
+	this.handleMouseUp = this.handleMouseUp.bind(this);
+	this.handleDragStart = this.handleDragStart.bind(this);
+	this.handleDragEnd = this.handleDragEnd.bind(this);
+
+	document.onkeypress = this.handleKeyPress;
+	document.onkeyup = this.ls.saveAfter(function(){});
+	document.onclick = this.ls.saveAfter(this.handleClick);
+	document.ondblclick = this.ls.saveAfter(this.handleDblClick);
+	document.onmousedown = this.handleMouseDown;
+	document.onmouseup = this.ls.saveAfter(this.handleMouseUp);
+	document.ondragstart = this.handleDragStart;
+	document.ondragend = this.ls.saveAfter(this.handleDragEnd);
+}
+
+Main.prototype.handleKeyPress = function(e) {
+	if (DEBUG_MODE) {
+		this.state.set("event/key/lastKey", e.key);
+	}
+};
+
+Main.prototype.handleClick = function(e) {
+	if (DEBUG_MODE) {
+		this.state.set("event/click/pos/client", [e.clientX, e.clientY]);
+		this.state.set("event/click/pos/page", [e.pageX, e.pageY]);
+		this.state.set("event/click/pos/screen", [e.screenX, e.screenY]);
+		this.state.set("event/click/target", e.target.nodeName);
+	}
+
+	switch(e.target.nodeName) {
+		case 'TEXTAREA':
+			break;
+		case 'BUTTON':
+			if (e.target.id === cnst("domId/clearBtn") &&
+				prompt("Are you sure to clear the board? [Type: YES]") == "YES") {
+				document.getElementById(cnst("domId/canvas")).innerHTML = "";
+				this.state.set("nodes", {});
+				this.ls.save();
+			}
+			break;
+		default:
+			// Create a node when mouse is left clicked.
+			new NodeView(undefined, [e.pageX, e.pageY], this.state);
+	}
+};
+
+Main.prototype.handleDblClick = function(e) {
+	if (e.target.nodeName === "TEXTAREA") {
+		var conf = confirm("You sure to delete this node "+e.target.id+"?\nContent: " + e.target.value);
+		if (conf) {
+			this.state.del("nodes/"+e.target.id);
+			e.target.remove();
+		}
+		
+	}
+}
+
+Main.prototype.handleMouseDown = function(e) {
+	if (e.target.nodeName === 'TEXTAREA') {
+		var borderX1 = parseInt(e.target.style.left) + parseInt(e.target.style.width);
+		var borderX2 = borderX1 - 20;
+		var borderY1 = parseInt(e.target.style.top) + parseInt(e.target.style.height);
+		var borderY2 = borderY1 - 20;
+
+		this.state.set("mouseDown/targetId", e.target.id);
+		this.state.set("mouseDown/bx1", borderX1);
+		this.state.set("mouseDown/bx2", borderX2);
+		this.state.set("mouseDown/by1", borderY1);
+		this.state.set("mouseDown/by2", borderY2);
+		this.state.set("mouseDown/client", [e.clientX, e.clientY]);
+
+		if (e.clientX >= borderX2 &&
+			e.clientY >= borderY2) {
+			this.state.set("mode", "resize");
+			this.state.set("resize/w0", e.target.style.width);
+			this.state.set("resize/h0", e.target.style.height);
+			e.target.style.width = 0;
+			e.target.style.height = 0;
+		} else {
+			this.state.set("mode", "drag");
+		}
+	}
+}
+
+Main.prototype.handleMouseUp = function(e) {
+	if (this.state.get("mode") === "resize") {
+		var w0 = state.get("resize/w0");
+		var h0 = state.get("resize/h0");
+		var nodeId = state.get("mouseDown/targetId");
+		var w1 = document.getElementById(nodeId).style.width;
+		var h1 = document.getElementById(nodeId).style.height;
+		if (w0 !== w1 || h0 != h1){
+			console.log("trigger textarea resize!");
+		}
+		this.state.set("mode", "idle");
+		this.state.set("resize/w0", undefined);
+		this.state.set("resize/h0", undefined);
+		this.state.set("mouseDown/targetId", undefined);
+	}
+}
+
+
+Main.prototype.handleDragStart = function(e) {
+	var offX = e.clientX - parseInt(e.target.style.left, 10) ;
+	var offY = e.clientY - parseInt(e.target.style.top, 10);
+
+	this.state.set("drag/offX", offX);
+	this.state.set("drag/offY", offY);
+}
+
+Main.prototype.handleDragEnd = function(e) {
+	if (DEBUG_MODE) {
+		this.state.set("mode", "idle");
+	}
+
+	var x = e.pageX - this.state.get("drag/offX");
+	var y = e.pageY - this.state.get("drag/offY");
+
+	e.target.style.left = x + 'px';
+	e.target.style.top = y + 'px';
+}
+
+
+/*************************************************************
+* NODE VIEW MODULE
+*************************************************************/
+
+function NodeView(nodeId, position, state) {
 	var position = (position === undefined) ? [0,0] : position;
 	var nodeId =  (nodeId === undefined) ? nextId() : nodeId;
-	setStt("nodes/"+nodeId, this);
+	assert(state !== undefined, "No [state] passed to NodeView");
+	state.set("nodes/"+nodeId, this);
 
 	this.view = document.createElement('textarea');
 	this.view.style.position = "absolute";
@@ -19,11 +165,10 @@ function NodeView(nodeId, position) {
 	this.view.draggable = true;
 	this.view.id = nodeId;
 	this.view.style.minWidth = "50px";
-	// this.view.style.minHeight = "50px";
 	this.view.style.resize = "both";
 
 
-	document.getElementById(CANVAS_DOMID).append(this.view);
+	document.getElementById(cnst('domId/canvas')).append(this.view);
 	this.view.focus();
 
 
@@ -35,156 +180,24 @@ function NodeView(nodeId, position) {
 			for (var i = 0; i < len; i++) {
 				id += chars[Math.floor(Math.random() * chars.length)];
 			}
-		} while (id in getStt("nodes"));
+		} while (id in state.get("nodes"));
 		return id;
 	}
 }
 
-function main() {
-	// localStorage.clear();
 
-	// SET INITIAL STATE
-	setStt("nodes", {});
-	setStt("mouseDown/targetId", null);
-	setStt("mode", "idle");
+/*************************************************************
+* LOCAL STORAGE MODULE
+*************************************************************/
 
-	// LOAD FROM LOCAL STORAGE
-	load();
+function LocalStorage(state, domId) {
+	this.state = state;
+	this.DOM_ID = domId;
+}
 
-	// AUTOSAVE
-	setInterval(save, 1000);
-
-	document.onkeypress = function(e) {
-		// DEBUG
-		setStt("event/key/lastKey", e.key);
-	};
-
-	document.ondblclick = function(e) {
-		if (e.target.nodeName === "TEXTAREA") {
-			var conf = confirm("You sure to delete this node "+e.target.id+"?\nContent: " + e.target.value);
-			if (conf) {
-				delStt("nodes/"+e.target.id);
-				e.target.remove();
-			}
-			
-		}
-	}
-
-	document.onclick = function(e) {
-		// DEBUG
-		setStt("event/click/pos/client", [e.clientX, e.clientY]);
-		setStt("event/click/pos/page", [e.pageX, e.pageY]);
-		setStt("event/click/pos/screen", [e.screenX, e.screenY]);
-		setStt("event/click/target", e.target.nodeName);
-
-		switch(e.target.nodeName) {
-			case 'TEXTAREA':
-				break;
-			case 'BUTTON':
-				break;
-			default:
-				// Create a node when mouse is left clicked.
-				new NodeView(undefined, [e.pageX, e.pageY]);
-		}
-	};
-
-	document.onmousedown = function(e) {
-		if (e.target.nodeName === 'TEXTAREA') {
-			var borderX1 = parseInt(e.target.style.left) + parseInt(e.target.style.width);
-			var borderX2 = borderX1 - 20;
-			var borderY1 = parseInt(e.target.style.top) + parseInt(e.target.style.height);
-			var borderY2 = borderY1 - 20;
-			setStt("mouseDown/targetId", e.target.id);
-			setStt("mouseDown/bx1", borderX1);
-			setStt("mouseDown/bx2", borderX2);
-			setStt("mouseDown/by1", borderY1);
-			setStt("mouseDown/by2", borderY2);
-			setStt("mouseDown/client", [e.clientX, e.clientY]);
-
-
-			if (e.clientX >= borderX2 &&
-				e.clientY >= borderY2) {
-				setStt("mode", "resize");
-				setStt("resize/w0", e.target.style.width);
-				setStt("resize/h0", e.target.style.height);
-				e.target.style.width = 0;
-				e.target.style.height = 0;
-			} else {
-				setStt("mode", "drag");
-			}
-
-			
-
-
-			// e.target.style.width = 0;
-			// e.target.style.height = 0;
-		}
-	}
-	document.onmousemove = function (e) {
-		// var targetId = getStt("mouseDown/targetId");
-		// if (targetId) {
-		// 	switch(getStt("mode")) {
-		// 		case "resize":
-		// 			target = document.getElementById(targetId);
-		// 			// target.style.width = 0;
-		// 			// target.style.height = 0;
-		// 			target.style.width = e.clientX - target.style.left;
-		// 			target.style.height = e.clientY - target.style.top;
-		// 			break;
-		// 	}
-		// }
-	}
-	document.onmouseup = function(e) {
-		if (getStt("mode") === "resize") {
-			var w0 = getStt("resize/w0");
-			var h0 = getStt("resize/h0");
-			var nodeId = getStt("mouseDown/targetId");
-			var w1 = document.getElementById(nodeId).style.width;
-			var h1 = document.getElementById(nodeId).style.height;
-			if (w0 !== w1 || h0 != h1){
-				console.log("trigger textarea resize!");
-			}
-			setStt("mode", "idle");
-			setStt("resize/w0", undefined);
-			setStt("resize/h0", undefined);
-			setStt("mouseDown/targetId", undefined);
-		}
-	}
-
-	document.ondragstart = function(e) {
-		var offX = e.clientX - parseInt(e.target.style.left, 10) ;
-		var offY = e.clientY - parseInt(e.target.style.top, 10);
-
-		setStt("drag/offX", offX);
-		setStt("drag/offY", offY);
-	}
-
-	document.ondrag = function(e) {
-		// DEBUG
-		// setStt("drag/target", e.target);
-		// setStt("drag/pos/client", [e.clientX, e.clientY]);
-		// setStt("drag/pos/page", [e.pageX, e.pageY]);
-		// setStt("drag/pos/screen", [e.screenX, e.screenY]);
-	}
-
-	document.ondragend = function(e) {
-		// DEBUG
-		setStt("mode", "idle");
-
-		var x = e.pageX - getStt("drag/offX");
-		var y = e.pageY - getStt("drag/offY");
-
-		e.target.style.left = x + 'px';
-		e.target.style.top = y + 'px';
-
-
-	}
-
-};
-
-function save() {
+LocalStorage.prototype.save = function() {
 	// save nodes: id, size, position, content
-	var sttNodes = getStt("nodes");
+	var sttNodes = this.state.get("nodes");
 	var jsonNodes = {};
 
 	for (var sttNodeId in sttNodes) {
@@ -199,21 +212,33 @@ function save() {
 	localStorage.setItem("nodes", JSON.stringify(jsonNodes));
 
 	var now = new Date();
-	document.getElementById("message").innerHTML = "Saved " + now.getHours() + ":" + now.getMinutes();
-
+	document.getElementById("message").innerHTML = "Saved to localStorage!";
+	setTimeout(function(){
+		document.getElementById("message").innerHTML = "";
+	},500);
 
 }
 
-function load() {
+LocalStorage.prototype.saveAfter = function(f) {
+	var ls = this;
+	function wrap() {
+		f.apply(null, arguments);
+		ls.save();
+	}
+	return wrap;
+	
+}
+
+LocalStorage.prototype.load = function() {
 	var sttNodes = {};
 	var jsonNodes = JSON.parse(localStorage.getItem("nodes"));
 
 	// Clear Canvas VIEW
-	document.getElementById(CANVAS_DOMID).innerHTML = "";
+	document.getElementById(this.DOM_ID).innerHTML = "";
 
 	for (var jsonNodeId in jsonNodes) {
 		var jsonNode = jsonNodes[jsonNodeId];
-		var sttNode = new NodeView(jsonNodeId);
+		var sttNode = new NodeView(jsonNodeId, undefined, this.state);
 		sttNode.view.style.width = jsonNode.size[0];
 		sttNode.view.style.height = jsonNode.size[1];
 		sttNode.view.style.left = jsonNode.position[0];
@@ -221,39 +246,47 @@ function load() {
 		sttNode.view.value = jsonNode.content;
 		sttNodes[jsonNodeId] = sttNode;
 	}
-	setStt("nodes", sttNodes);
+	this.state.set("nodes", sttNodes);
 
 }
 
 
-function setStt(rawKey, val) {
-	var keys = rawKey.split('/');
+/*************************************************************
+* STATE MODULE
+*************************************************************/
 
-	var parent = STATES;
+function State(domId) {
+	this.STATES = {};
+	this.DOM_ID = domId;
+}
+
+State.prototype.set = function(rawKey, val) {
+	var keys = rawKey.split('/');
+	var parent = this.STATES;
 	for (var i = 0; i < keys.length - 1; i++) {
 		parent[keys[i]] = parent[keys[i]] || {};
 		parent = parent[keys[i]];
 	}
 	parent[keys[keys.length - 1]] = val
-	dispStt();
+	this.display();
 }
 
-function getStt(rawKey) {
+State.prototype.get = function(rawKey) {
 	var keys = rawKey.split('/');
 
-	var parent = STATES;
+	var parent = this.STATES;
 	for (var i = 0; i < keys.length - 1; i++) {
 		assert(keys[i] in parent, "Key Not Found in State: ["+keys[i]+"] in ["+rawKey+"]")
 		parent = parent[keys[i]];
 	}
 	assert(keys[i] in parent, "Key Not Found in State: ["+keys[i]+"] in ["+rawKey+"]")
-	return parent[keys[i]]
+	return parent[keys[i]];
 }
 
-function delStt(rawKey) {
+State.prototype.del = function(rawKey) {
 	var keys = rawKey.split('/');
 
-	var parent = STATES;
+	var parent = this.STATES;
 	for (var i = 0; i < keys.length - 1; i++) {
 		assert(keys[i] in parent, "Key Not Found in State: ["+keys[i]+"] in ["+rawKey+"]")
 		parent = parent[keys[i]];
@@ -263,18 +296,32 @@ function delStt(rawKey) {
 	return true;
 }
 
-function dispStt() {
+State.prototype.display = function() {
 	var text = "STATES<br/>";
-	var sortedKeys = Object.keys(STATES).sort();
+	var sortedKeys = Object.keys(this.STATES).sort();
 	for (var i = 0; i < sortedKeys.length; i++) {
-		text += sortedKeys[i] + ": " + JSON.stringify(STATES[sortedKeys[i]]) + "<br/>";
+		text += sortedKeys[i] + ": " + JSON.stringify(this.STATES[sortedKeys[i]]) + "<br/>";
 	}
-	document.getElementById(STATES_DOMID).innerHTML = text + "\n\n" + JSON.stringify(localStorage);
-	// document.getElementById(STATES_DOMID).innerHTML = JSON.stringify(STATES);
+	document.getElementById(this.DOM_ID).innerHTML = text + "\n\n" + JSON.stringify(localStorage);
 }
+
+
+/*************************************************************
+* CUSTOM BUILT-IN HELPER FUNCTIONS
+*************************************************************/
 
 function assert(condition, message) {
 	if (!condition) {
 		throw message || "Assertion Error!";
 	}
+}
+
+function cnst(key) {
+	var constants = {
+		'domId/canvas': 'canvas',
+		'domId/states': 'states',
+		'domId/clearBtn': 'clearBtn',
+	}
+	assert(key in constants, "["+key+"] is not in constants.");
+	return constants[key];
 }
